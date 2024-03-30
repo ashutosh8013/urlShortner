6792c3dae1da487ff1c3562927c61fcda00f258b
ashutosh8013 <aj.ashutoshjha2000@gmail.com>
2024-03-30 10:31:09 +0530

    changes in google env

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId="423092457490-7e7n3i00p04jqb1to13pvo6m1n2fanah.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId="423092457490-porni7lrv6hgmsmgqqqk4ihqcg5kjppu.apps.googleusercontent.com">
     <Router>
       <App />
     </Router>
